0b88bc09492bfdb21766576e63b0f1cffcc7d4be
Gabriel Seabra <gdseabra@gmail.com>
2021-08-13 15:19:23 -0300

    add clickable samples in projection

--- projection_window.py
+++ projection_window.py
@@ -1,3 +1,8 @@
+from random import sample
+from numpy import dtype
+from numpy.core.fromnumeric import reshape
+import utils
+import numpy as np
 # importing Qt widgets
 from PyQt5.QtWidgets import *
 
@@ -20,12 +25,13 @@ try:
 except:
     warnings.warn("PyIFT is not installed.", ImportWarning)
 
-class SamplePoint():
-	def __init__(self, id, x, y, img):
+class SamplePoint(pg.SpotItem):
+	def __init__(self, id, x, y, img, true_label):
 		self.id=id
 		self.x=x
 		self.y=y
 		self.img=img
+		self.true_label=true_label
 	
 	def print_info(self):
 		print("{")
@@ -34,7 +40,6 @@ class SamplePoint():
 		print("    y: ", self.y)
 		print("    img: ", self.img)
 		print("}")
-
 
 class ProjectionWindow(QMainWindow):
 
@@ -72,30 +77,43 @@ class ProjectionWindow(QMainWindow):
 		# making label do word wrap
 		label.setWordWrap(True)
 
+		training_data = utils.get_training_data()[:1000]
 
-		Z = utils.load_opf_dataset('../datasets/corel.zip')
+		X = training_data[:,0]
+		X_ = np.array([x.flatten() for x in X], dtype=np.float32)
+		# print(X_)
+		Y = np.array(training_data[:,1], dtype=np.int32)
+		paths = list(training_data[:,2])
+		ids = np.array(training_data[:,3], dtype=np.int32)
+		# y = np.ndarray([y_[0] for y_ in Y], dtype=np.int32)
+		Z = ift.CreateDataSetFromNumPy(X_, Y)
+		Z.SetId(ids)
+		Z.SetRefData(paths)
 
-		ds = Z.GetData()
-		sample_features = ds[5]
-		reduced_ds = ift.DimReductionByTSNE(Z, 2, 20, 1000)
-		print(Z.GetData()[10].shape)
+		print(Z.nsamples, " amostras")
+		reduced_ds = ift.DimReductionByTSNE(Z, 2, 30, 1000)
 		
 		proj_data = Z.GetProjection()
 		orig_data = Z.GetData()
+		ref_data = Z.GetRefData()
+		true_labels = Z.GetTrueLabels()
 		ids = Z.GetIds()
+		
 
-		sample_points = {}
+		self.sample_points = {}
 		for i in range(Z.nsamples):
 			img = orig_data[i]
-			p = SamplePoint(ids[i], proj_data[i][0], proj_data[i][1], None)
-			sample_points[str(ids[i])] = p
+			sample_x = proj_data[i][0]
+			sample_y = proj_data[i][1]
+			p = SamplePoint(ids[i], sample_x, sample_y, ref_data[i], true_labels[i])
+			self.sample_points[str(sample_x)+" "+str(sample_y)] = p
 		
 		# for key in sample_points:
 		# 	sample_points[key].print_info()
 		
 		# creating a plot window      
 		plot = pg.plot()
-  
+
      
   
         # creating a scatter plot item
@@ -103,12 +121,17 @@ class ProjectionWindow(QMainWindow):
         # using brush to enlarge the of green color
 		scatter = pg.ScatterPlotItem(size=10, brush=pg.mkBrush(30, 255, 35, 255))
 
+
   		# Convert data array into a list of dictionaries with the x,y-coordinates
 		# pos = [{'pos': tsne[:, i]} for i in range(len(tsne))]
 		# adding spots to the scatter plot
 		
-		for sample in proj_data:
-			scatter.addPoints(x=[sample[0]], y=[sample[1]])
+		for key in self.sample_points:
+			sample = self.sample_points[key]
+			if np.asscalar(sample.true_label)== 0:
+				scatter.addPoints([{'pos': (sample.x,sample.y), 'brush':'r'}])
+			else:
+				scatter.addPoints(x=[sample.x], y=[sample.y])
   
         # add item to plot window
         # adding scatter plot item to the plot window
@@ -132,3 +155,9 @@ class ProjectionWindow(QMainWindow):
 
 		# setting this widget as central widget of the main widow
 		self.setCentralWidget(widget)
+
+		scatter.sigClicked.connect(self.clicaste)
+	
+	def clicaste(self, obj, points):
+		key = str(points[0]._data['x']) + " " + str(points[0]._data['y'])
+		print("clicaste no ponto da imagem: ", self.sample_points[key].img)
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import warnings
+import os
+import numpy as np
+import cv2
+from tqdm import tqdm
+from random import shuffle
 ift = None
 
 try:
@@ -6,9 +11,51 @@ try:
 except:
     warnings.warn("PyIFT is not installed.", ImportWarning)
 
+TRAIN_DIR = 'train/'
+TEST_DIR = './test1/'
+IMG_SIZE = 50
+
 def load_opf_dataset(path):
     assert ift is not None, "PyIFT is not available"
 
     opf_dataset = ift.ReadDataSet(path)
 
     return opf_dataset
+
+def get_training_data():
+    '''
+        Return training data
+    '''
+    training_data = []
+    if os.path.isfile('training_data_{}.npy'.format(IMG_SIZE)):
+        return np.load('training_data_{}.npy'.format(IMG_SIZE), allow_pickle=True)
+    else:
+        for img in tqdm(os.listdir(TRAIN_DIR)):
+            label = get_label(img)
+            id = get_id(img)
+            path = os.path.join(TRAIN_DIR,img)
+            img = cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE), (IMG_SIZE,IMG_SIZE))
+            img = img/255
+            training_data.append([np.array(img),np.array(label), [path], np.array(id)])
+        shuffle(training_data)
+        np.save('training_data_{}.npy'.format(IMG_SIZE),training_data)
+        return np.array(training_data)
+
+def get_label(img):
+    '''
+        Return the label for images
+    '''
+    word = img.split('.')[0]
+    if word == 'cat':
+        return [0]
+    else:
+        return [1]
+
+def get_id(img):
+    '''
+        Return unique id for sample image
+    '''
+    num = int(img.split('.')[1]) + 1
+    label = get_label(img)
+
+    return num*(label[0] + 1)
