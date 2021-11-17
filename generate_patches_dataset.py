b8b9fdbc156dc7de697f5f843ad16ce97033e950
Gabriel Seabra <gdseabra@gmail.com>
2021-11-17 17:38:52 -0300

    fix patches dataset generator bug

--- generate_patches_dataset.py
+++ generate_patches_dataset.py
@@ -1,16 +1,21 @@
 ift = None
 import warnings
 import sys
+import shutil
 try:
     import pyift.pyift as ift
 except:
     warnings.warn("PyIFT is not installed.", ImportWarning)
 
 original_imgs_path = sys.argv[1]
-input_mimgs_path = sys.argv[2]
-output_dir = sys.argv[3]
-nsuperpixels = int(sys.argv[4])
-patch_size = int(sys.argv[5])
+output_dir = sys.argv[2]
+nsuperpixels = int(sys.argv[3])
+patch_size = int(sys.argv[4])
 
-Z = ift.PatchesFromSuperpixels(original_imgs_path, input_mimgs_path, output_dir, nsuperpixels, patch_size)
+Z = ift.PatchesFromSuperpixels(original_imgs_path, output_dir, nsuperpixels, patch_size)
 print(Z.nsamples)
+
+try:
+    shutil.rmtree(output_dir.split('.')[0])
+except OSError as e:
+    print("Error: %s - %s." % (e.filename, e.strerror))
